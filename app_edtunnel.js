e5324dea6b4907f1c6fa4b83f5bdad4802037d52
JYPSM <140132963+JYPSM@users.noreply.github.com>
2023-07-30 17:06:34 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'bbafb487-6e50-46e5-9efe-de8be0ec2cc4';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
